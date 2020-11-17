7600c9e35621c15ba405dd7c648d568b2170f57e
Kamil Duliniec <123dulko@wp.pl>
2020-11-17 21:49:18 +0100

    register form complete

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,10 +1,51 @@
 import React from "react";
 import Welcome from "../Welcome/Welcome";
+import user from "../../assets/login.png";
+import password from "../../assets/password.png";
+import email from "../../assets/email.png";
 
 export default function Register() {
   return (
-    <div>
+    <div className="register">
       <Welcome />
+      <div className="register__form">
+        <h3>Register</h3>
+        <form>
+          <label>
+            <span>
+              <img src={user} className="register__span" />
+            </span>
+            <input type="text" placeholder="enter your login" />
+          </label>
+          <label>
+            <span>
+              <img src={password} className="register__span" />
+            </span>
+            <input type="text" placeholder="enter your password" />
+          </label>
+          <label>
+            <span>
+              <img src={password} className="register__span" />
+            </span>
+            <input type="text" placeholder="repeat your password" />
+          </label>
+          <label>
+            <span>
+              <img src={email} className="register__span" />
+            </span>
+            <input type="text" placeholder="enter your email" />
+          </label>
+          <div className="register__checkbox">
+            <input type="checkbox" />
+            <span>Remember me!</span>
+          </div>
+          <div className='register__buttonBox'>
+            <button type="submit" className="register__button">
+              Sign Up
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
