f0f430bd429312ccc180cd3d2ec26eb023124789
Kamil Duliniec <123dulko@wp.pl>
2020-11-19 10:44:41 +0100

    set useState for register, initialize firebase and auth in signup

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,42 +1,100 @@
-import React from "react";
+import React, { useState } from "react";
 import Welcome from "../Welcome/Welcome";
 import user from "../../assets/login.png";
 import password from "../../assets/password.png";
 import email from "../../assets/email.png";
+import fire from "../Firebase/firebaseConfig";
 
 export default function Register() {
+  const [state, setState] = useState({
+    userName: "",
+    email: "",
+    pass: "",
+    secondPass: "",
+    isChecked: false,
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleCheckbox = () => {
+    setState((prevState) => ({
+      ...prevState,
+      isChecked: !state.isChecked,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    fire
+      .auth()
+      .createUserWithEmailAndPassword(state.email, state.pass)
+      .then((info) => {
+        console.log(info);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className="register">
       <Welcome />
       <div className="register__form">
         <h3>Register</h3>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label>
             <span>
-              <img src={user} className="register__span" />
+              <img src={user} className="register__span" alt="login user" />
             </span>
-            <input type="text" placeholder="enter your login" />
+            <input
+              name="userName"
+              type="text"
+              placeholder="enter your login"
+              onChange={handleChange}
+            />
           </label>
           <label>
             <span>
-              <img src={password} className="register__span" />
+              <img src={password} className="register__span" alt="password" />
             </span>
-            <input type="text" placeholder="enter your password" />
+            <input
+              name="pass"
+              type="password"
+              placeholder="enter your password"
+              onChange={handleChange}
+            />
           </label>
           <label>
             <span>
-              <img src={password} className="register__span" />
+              <img src={password} className="register__span" alt="password" />
             </span>
-            <input type="text" placeholder="repeat your password" />
+            <input
+              name="secondPass"
+              type="password"
+              placeholder="repeat your password"
+              onChange={handleChange}
+            />
           </label>
           <label>
             <span>
-              <img src={email} className="register__span" />
+              <img src={email} className="register__span" alt="email" />
             </span>
-            <input type="text" placeholder="enter your email" />
+            <input
+              name="email"
+              type="email"
+              placeholder="enter your email"
+              onChange={handleChange}
+            />
           </label>
           <div className="register__checkbox">
-            <input type="checkbox" />
+            <input type="checkbox" name="isChecked" onChange={handleCheckbox} />
             <span>Remember me!</span>
           </div>
           <button type="submit" className="register__button">
