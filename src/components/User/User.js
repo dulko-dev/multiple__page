078d65e2abf83d885d3b224a1bfc7c9a9ac88be1
Kamil Duliniec <123dulko@wp.pl>
2020-11-25 23:49:59 +0100

    fetch Name Day from API, added time-zone and date by Object

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import fire from "../Firebase/firebaseConfig";
 import Applications from "./Applications";
 
 function User({ user }) {
+  const [nameDay, setNameDay] = useState("");
+  const [watch, setWatch] = useState("");
+
   const handleLogOut = (e) => {
     e.preventDefault();
-
     fire
       .auth()
       .signOut()
@@ -17,13 +19,50 @@ function User({ user }) {
       });
   };
 
+  useEffect(() => {
+    setInterval(() => {
+      const d = new Date().toLocaleTimeString();
+      setWatch(d);
+    }, 1000);
+  }, []);
+
+  useEffect(() => {
+    const takeName = () => {
+      fetch("https://api.abalin.net/today")
+        .then((response) => {
+          if (response) {
+            return response;
+          } else {
+            const errMessage = `An error has occurred : ${response.status}`;
+            throw Error(errMessage);
+          }
+        })
+        .then((res) => res.json())
+        .then((data) => {
+          setNameDay(data.data.namedays.pl);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    takeName();
+  }, []);
+
+  const time = new Date().toLocaleDateString("en-GB", {
+    weekday: "long",
+    day: "numeric",
+    year: "numeric",
+    month: "long",
+  });
+
   return (
     <div className="user">
       <div className="user__nav">
-        <div>
+        <div className="user__date">
           <h2>Welcome {user}</h2>
-          <p>Today is 24 November 2020</p>
-          <p>TimeZone : 12:33:02 AM</p>
+          <p>Today is : {time} </p>
+          <p>Name-day have - {nameDay}</p>
+          <p>TimeZone : {watch}</p>
         </div>
         <div className="user__buttons">
           <button type="button" className="user__nav__button">
