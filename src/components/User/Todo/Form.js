8dc55b9a139da451afbea61b5f958975102ea181
Kamil Duliniec <123dulko@wp.pl>
2020-11-30 22:31:39 +0100

    added style for input and button in todo components

--- src/components/User/Todo/Form.js
+++ src/components/User/Todo/Form.js
@@ -26,11 +26,27 @@ const Form = ({ setInputText, setTodos, inputText, todos, setStatus }) => {
   };
   return (
     <div className="form__inside">
-      <h2>Your goals to step forward to better future:</h2>
+      <h2>
+        <span>Your goals to step forward</span>
+        <span>to the better future: </span>
+      </h2>
       <form onSubmit={addedHandler}>
-        <input value={inputText} onChange={onChangeHandler} type="text" />
-        <button>Add to list</button>
-        <select onChange={statusHandler}>
+        <div className="form__group field">
+          <input
+            onChange={onChangeHandler}
+            value={inputText}
+            type="input"
+            className="form__field"
+            placeholder="Add task"
+            id="task"
+            required
+          />
+          <label htmlFor="task" className="form__label">
+            Add task
+          </label>
+        </div>
+        <button className="form__button">Add to list</button>
+        <select onChange={statusHandler} className="form__select">
           <option value="all">all</option>
           <option value="completed">completed</option>
           <option value="uncompleted">uncompleted</option>
