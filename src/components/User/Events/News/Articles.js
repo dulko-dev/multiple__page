09aa0aa07e27864aa351040d4ab6dbdc4f9add1c
Kamil Duliniec <123dulko@wp.pl>
2021-01-14 12:19:18 +0100

    delete yellow errors

--- src/components/User/Events/News/Articles.js
+++ src/components/User/Events/News/Articles.js
@@ -1,14 +1,13 @@
 import React from "react";
-import noPhoto from "../../../../assets/no-photo.png";
 
 function Articles({ element }) {
   return (
     <div className="articles">
       <h3 className="articles__title">"{element.title}"</h3>
-      <a className="articles__text" href={element.url} target="_blank">
+      <a className="articles__text" href={element.url} target="_blank" rel='noreferrer'>
         <div className="articles__img">
           {element.media.map((media) => (
-            <img
+            <img key={media}
               style={{ width: "400px" }}
               src={media["media-metadata"][2].url}
               alt={media["media-metadata"].caption}
--- src/components/User/User.js
+++ src/components/User/User.js
@@ -4,7 +4,7 @@ import Applications from "./Applications/Applications";
 import Support from "../Utility/Support/Support";
 
 export default function User() {
-  const [homePage, setHomePage] = useState("none");
+  const [homePage] = useState("none");
   return (
     <div>
       <UserNav homePage={homePage} />
