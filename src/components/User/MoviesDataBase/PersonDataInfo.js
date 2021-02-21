fc0925de21e178a38e0170c52dd79302f32fb8ad
Kamil Duliniec <123dulko@wp.pl>
2021-02-21 14:09:56 +0100

    fixed margin into movieDataBase components

--- src/components/User/MoviesDataBase/PersonDataInfo.js
+++ src/components/User/MoviesDataBase/PersonDataInfo.js
@@ -40,7 +40,7 @@ function PersonDataInfo({ datas, input }) {
         known for:
         {datas.known_for.map((movie) => (
           <span key={movie.id} style={{ color: "#d5ac4e", paddingLeft: "5px" }}>
-            {movie.title} 
+            {movie.title}
           </span>
         ))}
       </p>
