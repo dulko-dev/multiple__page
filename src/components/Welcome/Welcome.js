1f8306b67de3269dd48c996f2a44e3b5d6263ec8
Kamil Duliniec <123dulko@wp.pl>
2021-02-25 13:20:18 +0100

    delete render animation in Welcome component - useless

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,82 +1,74 @@
-import React, { useContext, lazy, Suspense } from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { AuthContext } from "../Firebase/Auth";
-// import User from "../User/User";
+import User from "../User/User";
 import login from "../../assets/login.png";
 import plus from "../../assets/add.png";
 import bg from "../../assets/fog-city2.jpg";
 import bgWebp from "../../assets/webP/fog-city2.webp";
 
-
-const User = lazy(() => import("../User/User"));
-const loading = () => <div className="loader"></div>;
-
 export default function Welcome() {
   const { czoko } = useContext(AuthContext);
   const loaded = czoko;
 
-  return (
-    <Suspense fallback={loading()}>
-      {!loaded ? (
-        <User />
-      ) : (
-        <div className="welcome">
-          <picture>
-            <source srcSet={bgWebp} type="image/webp" />
-            <source srcSet={bg} type="images/jpg" />
-            <img
-              src={bg}
-              alt="background"
-              style={{
-                position: "fixed",
-                width: "100%",
-                height: "100%",
-                left: "0",
-                bottom: "0",
-                zIndex: "-1",
-              }}
-            />
-          </picture>
+  return !loaded ? (
+    <User />
+  ) : (
+    <div className="welcome">
+      <picture>
+        <source srcSet={bgWebp} type="image/webp" />
+        <source srcSet={bg} type="images/jpg" />
+        <img
+          src={bg}
+          alt="background"
+          style={{
+            position: "fixed",
+            width: "100%",
+            height: "100%",
+            left: "0",
+            bottom: "0",
+            zIndex: "-1",
+          }}
+        />
+      </picture>
 
-          <h1>Welcome your Dashboard</h1>
-          <div className="welcome__icons">
-            <NavLink
-              className="welcome__navlink"
-              to="/login"
-              activeStyle={{
-                borderRadius: "60%",
-                border: "4px solid gold",
-                outline: "none",
-              }}
-            >
-              <div>
-                <img
-                  src={login}
-                  alt="login"
-                  style={{ width: "50px", height: "50px" }}
-                />
-              </div>
-            </NavLink>
-            <NavLink
-              to="/register"
-              className="welcome__navlink"
-              activeStyle={{
-                borderRadius: "60%",
-                border: "4px solid gold",
-                outline: "none",
-              }}
-            >
-              <div>
-                <img
-                  src={plus}
-                  alt="add user"
-                  style={{ width: "50px", height: "50px" }}
-                />
-              </div>
-            </NavLink>
+      <h1>Welcome your Dashboard</h1>
+      <div className="welcome__icons">
+        <NavLink
+          className="welcome__navlink"
+          to="/login"
+          activeStyle={{
+            borderRadius: "60%",
+            border: "4px solid gold",
+            outline: "none",
+          }}
+        >
+          <div>
+            <img
+              src={login}
+              alt="login"
+              style={{ width: "50px", height: "50px" }}
+            />
           </div>
-        </div>
-      )}
-    </Suspense>
+        </NavLink>
+        <NavLink
+          to="/register"
+          className="welcome__navlink"
+          activeStyle={{
+            borderRadius: "60%",
+            border: "4px solid gold",
+            outline: "none",
+          }}
+        >
+          <div>
+            <img
+              src={plus}
+              alt="add user"
+              style={{ width: "50px", height: "50px" }}
+            />
+          </div>
+        </NavLink>
+      </div>
+    </div>
   );
 }
